8147724c9da762e01d86c1724d3bc3fe87ee947f
Marina Toledo <marinatoledo@Marinas-MacBook-Air.local>
2016-12-31 20:18:54 -0200

    Personalizado

--- js/helper.js
+++ js/helper.js
@@ -1,17 +1,5 @@
-/*
-
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-
-
-/*
-These are HTML strings. As part of the course, you'll be using JavaScript functions
-replace the %data% placeholder text you see in them.
-*/
+/*These are HTML strings. As part of the course, you'll be using JavaScript
+ functions replace the %data% placeholder text you see in them.*/
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr>';
 
@@ -55,25 +43,23 @@ var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
 
-var internationalizeButton = '<button>Internationalize</button>';
+//var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
 
 
 /*
 The Internationalize Names challenge found in the lesson Flow Control from JavaScript Basics requires you to create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
 */
-$(document).ready(function() {
+/*$(document).ready(function() {
   $('button').click(function() {
     var $name = $('#name');
     var iName = inName($name.text()) || function(){};
     $name.html(iName);
   });
-});
+});*/
 
-/*
-The next few lines about clicks are for the Collecting Click Locations quiz in the lesson Flow Control from JavaScript Basics.
-*/
-var clickLocations = [];
+/*The next few lines about clicks are for the Collecting Click Locations quiz in the lesson Flow Control from JavaScript Basics.*/
+/*var clickLocations = [];
 
 function logClicks(x,y) {
   clickLocations.push(
@@ -86,22 +72,17 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
-});
+  logClicks(loc.pageX,loc.pageY);
+});*/
 
 
 
-/*
-This is the fun part. Here's where we generate the custom Google Map for the website.
-See the documentation below for more details.
-https://developers.google.com/maps/documentation/javascript/reference
-*/
+/* Here's where we generate the custom Google Map for the website.
+https://developers.google.com/maps/documentation/javascript/reference*/
 var map;    // declares a global map variable
 
 
-/*
-Start here! initializeMap() is called when page is loaded.
-*/
+/* Start here! initializeMap() is called when page is loaded.*/
 function initializeMap() {
 
   var locations;
@@ -110,49 +91,31 @@ function initializeMap() {
     disableDefaultUI: true
   };
 
-  /*
-  For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js.
-  */
-  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+  /* For the map to be displayed, the googleMap var must be
+  appended to #mapDiv in resumeBuilder.js. */
+  map = new google.maps.Map(document.querySelector('#mapDiv'), mapOptions);
 
-
-  /*
-  locationFinder() returns an array of every location string from the JSONs
-  written for bio, education, and work.
-  */
+  /* locationFinder() returns an array of every location string from the JSONs
+  written for bio, education, and work.*/
   function locationFinder() {
-
-    // initializes an empty array
     var locations = [];
 
-    // adds the single location property from bio to the locations array
     locations.push(bio.contacts.location);
 
-    // iterates through school locations and appends each location to
-    // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide:
-    // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
     education.schools.forEach(function(school){
-      locations.push(school.location);
+      locations.push(school.city);
     });
 
-    // iterates through work locations and appends each location to
-    // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide:
-    // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    work.jobs.forEach(function(job){
+    /* work.jobs.forEach(function(job){
       locations.push(job.location);
-    });
+    });*/
 
     return locations;
   }
 
-  /*
-  createMapMarker(placeData) reads Google Places search results to create map pins.
+  /* createMapMarker(placeData) reads Google Places search results to create map pins.
   placeData is the object returned from search results containing information
-  about a single location.
-  */
+  about a single location. */
   function createMapMarker(placeData) {
 
     // The next lines save location data from the search result object to local variables
@@ -177,7 +140,7 @@ function initializeMap() {
 
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+      infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -189,20 +152,16 @@ function initializeMap() {
     map.setCenter(bounds.getCenter());
   }
 
-  /*
-  callback(results, status) makes sure the search returned results for a location.
-  If so, it creates a new map marker for that location.
-  */
+  /* callback(results, status) makes sure the search returned results for a location.
+  If so, it creates a new map marker for that location. */
   function callback(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
       createMapMarker(results[0]);
     }
   }
 
-  /*
-  pinPoster(locations) takes in the array of locations created by locationFinder()
-  and fires off Google place searches for each location
-  */
+  /* pinPoster(locations) takes in the array of locations created by locationFinder()
+  and fires off Google place searches for each location */
   function pinPoster(locations) {
 
     // creates a Google place search service object. PlacesService does the work of
@@ -211,13 +170,12 @@ function initializeMap() {
 
     // Iterates through the array of locations, creates a search object for each location
       locations.forEach(function(place){
-      // the search request object
-      var request = {
+
+      var request = { // the search request object
         query: place
       };
 
-      // Actually searches the Google Maps API for location data and runs the callback
-      // function with the search results after each search.
+      // Actually searches the Google Maps API for location data and runs the callback function with the search results after each search.
       service.textSearch(request, callback);
     });
   }
@@ -225,25 +183,18 @@ function initializeMap() {
   // Sets the boundaries of the map based on pin locations
   window.mapBounds = new google.maps.LatLngBounds();
 
-  // locations is an array of location strings returned from locationFinder()
-  locations = locationFinder();
+  locations = locationFinder();// locations is an array of location strings
 
-  // pinPoster(locations) creates pins on the map for each location in
-  // the locations array
+  // pinPoster(locations) creates pins on the map for each location in the locations array
   pinPoster(locations);
 
 }
 
-/*
-Uncomment the code below when you're ready to implement a Google Map!
-*/
-
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
-// Vanilla JS way to listen for resizing of the window
-// and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+// Vanilla JS way to listen for resizing of the window and adjust map bounds
+window.addEventListener('resize', function(e) {
+//Make sure the map bounds get updated on page resize
+map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,234 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+    "name": "Marina Toledo",
+    "role": "Web developer",
+    "contacts": {
+        "mobile": "97776-9940",
+        "email": "marina.mi.toledo@hotmail.com",
+        "github": "marina-toledo",
+        "location": "São Paulo - Brazil"
+    },
+    "welcomeMessage": "Welcome!",
+    "skills": ["web", "Java", "HTML", "CSS", "Javascript", "UML"],
+    "bioPic": "images/bioPic.jpg"
+};
+
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+$("#header").prepend(formattedRole);
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+$("#header").prepend(formattedName);
+
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+$("#topContacts").append(formattedMobile);
+
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+$("#topContacts").append(formattedEmail);
+
+var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.github);
+$("#topContacts").append(formattedGitHub);
+
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+$("#topContacts").append(formattedLocation);
+
+var formattedBioPic = HTMLbioPic.replace("%data%", "images/bioPic.jpg");
+$("#header").append(formattedBioPic);
+
+var formattedWelcMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+$("#header").append(formattedWelcMsg);
+
+if( bio.skills.length > 0){
+    $("#header").append(HTMLskillsStart);
+
+    for( i=0; i < bio.skills.length; i++){
+        var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+        $("#skills").append(formattedSkill);
+    }
+
+}
+
+
+var work = {
+    "jobs":[
+    {
+        "employer": "Maps",
+        "title": "Java developer",
+        "dates": "2015-2016",
+        "description": "Develop, maintain, test and support for a web pricing software at Maps S.A. company with the technologies: Java, Groovy, IntelliJ, Mercurial, Spring, Mockito, JUnit, Jenkins, XML, Angular, PostgreSQL, Excel, and others. The features demanded front-end and back-end services, agile process management and use of Kanban dashboard."
+    },
+    {
+        "employer": "Laboratório de Sitemas Integráveis Tecnológico (LSI-TEC)",
+        "title": "Development analyst (Mobile tester)",
+        "dates": "2015",
+        "description": "Analyze, test and customize software for mobile phones. Use of technologies: Java, Git, XML and cellular transmission technologies (GSM, SMS / MMS / WAP, 3G)."
+    },
+    {
+        "employer": "LUZ Engenharia Financeira",
+        "title": "Risk analyst",
+        "dates": "2014",
+        "description": "Development of desktop application for credit risk analysis in the Delphi language."
+    },
+    {
+        "employer": "Touch Health",
+        "title": "Java web programmer internship",
+        "dates": "2012-2013",
+        "description": "Internship for creating a medical website with the technologies: Java, Eclipse, Maven, Hudson, Nexus, Liquibase, postgreSQL, Jenkins, with the Spring, Struts, JUnit and Hibernate frameworks in the company Touch Tecnologia e Informática LTDA."
+    },
+    {
+        "employer": "Instituto Nacional de Pesquisas Espaciais (INPE)",
+        "title": "Programmer and requirements analyst internship",
+        "dates": "2011-2012",
+        "description": "Internship for programming in Labview and constructing UML Diagrams in the GLOW project (assembly and creation of satellite launch software) at the National Institute for Space Research."
+    }
+    ]
+};
+
+function displayWork(){
+    for(job in work.jobs){
+        $("#workExperience").append(HTMLworkStart);
+
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[job].title);
+
+        var formattedEmployerTitle = formattedEmployer + formattedTitle;
+        $(".work-entry:last").append(formattedEmployerTitle);
+
+        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+        $(".work-entry:last").append(formattedDates);
+
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+        $(".work-entry:last").append(formattedDescription);
+    }
+};
+
+displayWork();
+
+
+var projects = {
+    "projects": [
+    {
+        "title": "sample project 1",
+        "dates": "2016",
+        "description": "web project",
+        "images": [
+                    "http://data.whicdn.com/images/22565834/large.jpg",
+                    "https://files.graphiq.com/stories/t2/tiny_cat_12573_8950.jpg"]
+    }
+    ]
+};
+
+function displayProjects(){
+    for(project in projects.projects){
+        $("#projects").append(HTMLprojectStart);
+
+        var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+        $(".project-entry:last").append(formattedTitle);
+
+        var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+        $(".project-entry:last").append(formattedDates);
+        var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+        $(".project-entry:last").append(formattedDescription);
+
+        if(projects.projects[project].images.length > 0){
+            for(image in projects.projects[project].images){
+                var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+                $(".project-entry:last").append(formattedImage);
+            }
+        }
+    }
+};
+
+displayProjects();
+
+
+var education = {
+    "schools":[
+    {
+        "name": "UNIFESP",
+        "city": "São José dos Campos, SP, Brazil",
+        "degree": "Bachelor",
+        "majors": ["CS"],
+        "dates": 2014,
+        "url": "http://www.sjc.unifesp.br/portal/"
+    },
+    {
+        "name": "Colégio Objetivo",
+        "city": "São José dos Campos, SP, Brazil",
+        "degree": "High School",
+        "majors": ["High School"],
+        "dates": 2008,
+        "url": "http://www.objetivo-sjc.com.br"
+    },
+    ],
+    "onlineCourses":[
+    {
+        "title": "Javascript Basics",
+        "school": "Udacity",
+        "dates": 2014,
+        "url": "https://br.udacity.com/course/javascript-basics--ud804/"
+    }
+    ]
+};
+
+function displayEducation(){
+    for(school in education.schools){
+        $("#education").append(HTMLschoolStart);
+
+        var formattedName = HTMLschoolName.replace("%data%", education.schools[school].name);
+        $("#education").append(formattedName);
+
+        var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+        $("#education").append(formattedDegree);
+
+        var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+        $("#education").append(formattedDates);
+
+        var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].city);
+        $("#education").append(formattedLocation);
+
+        var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[school].majors);
+        $("#education").append(formattedMajor);
+    }
+
+    for(onlineCourse in education.onlineCourses){
+        $("#education").append(HTMLonlineClasses);
+
+        var formattedTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[onlineCourse].title);
+        $("#education").append(formattedTitle);
+
+        var formattedSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[onlineCourse].school);
+        $("#education").append(formattedSchool);
+
+        var formattedDates = HTMLonlineDates.replace("%data%", education.onlineCourses[onlineCourse].dates);
+        $("#education").append(formattedDates);
+
+        var formattedURL = HTMLonlineURL.replace("%data%", education.onlineCourses[onlineCourse].url);
+        $("#education").append(formattedURL);
+    }
+};
+
+displayEducation();
+
+
+//$("#header").append(internationalizeButton);
+
+function inName(fullName){
+    var result = fullName.trim().split(' ');
+    result[0] = result[0][0].toUpperCase()+ result[0].slice(1).toLowerCase();
+    result[1] = result[1].toUpperCase();
+    fullName = result[0] + ' ' + result[1];
+    return fullName;
+};
+
+
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+$("#footerContacts").append(formattedMobile);
+
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+$("#footerContacts").append(formattedEmail);
+
+var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.github);
+$("#footerContacts").append(formattedGitHub);
+
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+$("#footerContacts").append(formattedLocation);
+
